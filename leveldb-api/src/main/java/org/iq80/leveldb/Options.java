71e95d70374acbc3fe77adc7b3eaf003f1a83d42
halibobo1205 <halibobo1205@gmail.com>
2022-12-28 20:11:37 +0800

    add FilterPolicy

--- leveldb-api/src/main/java/org/iq80/leveldb/Options.java
+++ leveldb-api/src/main/java/org/iq80/leveldb/Options.java
@@ -35,6 +35,21 @@ public class Options
     private long cacheSize;
     private int maxBatchSize = 80_000;
     private int maxManifestSize = 0; //M
+    // Return a new filter policy that uses a bloom filter with approximately
+    // the specified number of bits per key.  A good value for bits_per_key
+    // is 10, which yields a filter with ~ 1% false positive rate.
+    //
+    // Callers must delete the result after any database that is using the
+    // result has been closed.
+    //
+    // Note: if you are using a custom comparator that ignores some parts
+    // of the keys being compared, you must not use NewBloomFilterPolicy()
+    // and must provide your own FilterPolicy that also ignores the
+    // corresponding parts of the keys.  For example, if the comparator
+    // ignores trailing spaces, it would be incorrect to use a
+    // FilterPolicy (like NewBloomFilterPolicy) that does not ignore
+    // trailing spaces in keys.
+    private int bitsPerKey = 0;
 
     static void checkArgNotNull(Object value, String name)
     {
@@ -202,6 +217,17 @@ public class Options
             maxBatchSize(-1);
         }
         this.maxManifestSize = maxManifestSize;
+        return this;
+    }
+
+    public int bitsPerKey()
+    {
+        return bitsPerKey;
+    }
+
+    public Options bitsPerKey(int bitsPerKey)
+    {
+        this.bitsPerKey = bitsPerKey;
         return this;
     }
 }
